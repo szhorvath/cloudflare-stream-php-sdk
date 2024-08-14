5b2619b971c1142948caaf81d201a3f6a67dacab
Sandor Horvath <sandor.horvath@vivedia.net>
2024-08-14 21:15:58 +0100

    added webhook view and delete endpoints

--- src/Resources/Webhook/WebhookResource.php
+++ src/Resources/Webhook/WebhookResource.php
@@ -22,4 +22,27 @@ class WebhookResource extends Resource
             resultClass: Webhook::class
         );
     }
+
+    public function view(string $accountId): ApiResponse
+    {
+        $response = $this->client()->get(
+            uri: "/accounts/{$accountId}/stream/webhook"
+        );
+
+        return ApiResponse::from(
+            data: $this->decodeResponse($response),
+            resultClass: Webhook::class
+        );
+    }
+
+    public function delete(string $accountId): ApiResponse
+    {
+        $response = $this->client()->delete(
+            uri: "/accounts/{$accountId}/stream/webhook"
+        );
+
+        return ApiResponse::from(
+            data: $this->decodeResponse($response),
+        );
+    }
 }
--- tests/Resources/Webhook/WebhookResourceTest.php
+++ tests/Resources/Webhook/WebhookResourceTest.php
@@ -16,7 +16,7 @@ it('should return the webhook resource', function () {
     expect($output)->toBeInstanceOf(WebhookResource::class);
 });
 
-it('should verify token validity', function () {
+it('should create a webhook', function () {
     $client = new MockClient;
     $client->addResponse(response(
         status: Status::OK,
@@ -45,4 +45,54 @@ it('should verify token validity', function () {
         ->secret->toBe('e853f3bd4563a66c86979cd584f08eb3c72ddd75')
         ->modified->toBeInstanceOf(DateTimeImmutable::class)
         ->modified->format('Y-m-d H:i:s')->toBe('2024-08-14 09:13:00');
+});
+
+it('should view webhook', function () {
+    $client = new MockClient;
+    $client->addResponse(response(
+        status: Status::OK,
+        name: 'webhook/view',
+    ));
+
+    $sdk = new StreamSdk(
+        token: '123',
+        clientBuilder: mockBuilder($client)
+    );
+
+    $webhook = $sdk->webhook()->view(
+        accountId: '0a6c8c72a460f78152e767e10842dcb2',
+    );
+
+    expect($webhook)
+        ->toBeInstanceOf(ApiResponse::class)
+        ->success->toBeTrue();
+
+    expect($webhook->result)
+        ->notificationUrl->toBe('https://example.com/webhook')
+        ->notification_url->toBe('https://example.com/webhook')
+        ->secret->toBe('e853f3bd4563a66c86979cd584f08eb3c72ddd75')
+        ->modified->toBeInstanceOf(DateTimeImmutable::class)
+        ->modified->format('Y-m-d H:i:s')->toBe('2024-08-14 09:50:16');
+});
+
+it('should delete webhook', function () {
+    $client = new MockClient;
+    $client->addResponse(response(
+        status: Status::OK,
+        name: 'webhook/delete',
+    ));
+
+    $sdk = new StreamSdk(
+        token: '123',
+        clientBuilder: mockBuilder($client)
+    );
+
+    $webhook = $sdk->webhook()->delete(
+        accountId: '0a6c8c72a460f78152e767e10842dcb2',
+    );
+
+    expect($webhook)
+        ->toBeInstanceOf(ApiResponse::class)
+        ->success->toBeTrue()
+        ->result->toBeNull();
 });
